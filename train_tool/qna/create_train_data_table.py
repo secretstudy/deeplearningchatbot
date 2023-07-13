f981e49051ad45e4bb0000f778456484ab07e59e
secretaistudy <assassinaideveloper@gmail.com>
2023-07-13 19:05:29 +0900

    Create create_train_data_table.py

---
+++ train_tool/qna/create_train_data_table.py
@@ -0,0 +1,39 @@
+import pymysql
+from config.DatabaseConfig import * #DB 접속 정보 전부 가져오기
+
+db = None
+try:
+    print("DB 연결 시작 전")
+    db = pymysql.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        passwd=DB_PASSWORD,
+        db=DB_NAME,
+        charset='utf8'
+    )
+    print("DB 연결 성공")
+
+    # 학습 테이블 생성
+    sql = '''
+      CREATE TABLE IF NOT EXISTS `chatbot_train_data` (
+      `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      `intent` VARCHAR(45) NULL,
+      `ner` VARCHAR(1024) NULL,
+      `query` TEXT NULL,
+      `answer` TEXT NOT NULL,
+      `answer_image` VARCHAR(2048) NULL,
+      PRIMARY KEY (`id`))
+    ENGINE = InnoDB DEFAULT CHARSET=utf8
+    '''
+
+    # 쿼리 실행
+    with db.cursor() as cursor:
+        cursor.execute(sql)
+
+except Exception as e:
+    print(e)
+
+finally:
+    if db is not None:
+        db.close()
+        print("DB 연결 종료")
