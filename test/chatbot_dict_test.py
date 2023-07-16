bcb1e1ebdfbf7741a9d6d81b88f55b29de9c2048
secretaistudy <assassinaideveloper@gmail.com>
2023-07-16 22:01:47 +0900

    Update chatbot_dict_test.py

--- test/chatbot_dict_test.py
+++ test/chatbot_dict_test.py
@@ -5,9 +5,9 @@ f = open("../train_tools/dict/chatbot_dict.bin", "rb")
 word_index = pickle.load(f)
 f.close()
 
-#sent = "내일 오전 10시에 짬뽕 주문하고 싶어ㅋㅋ"
-sent = "원격지원  어떻게 해요?"
-#sent = "설치를 하고 싶은데 원격가능 할까요?"
+# sent = " 내일 오전 10시에 짬뽕 주문하고 싶어ㅋㅋ "
+sent = " 원격지원  어떻게 해요? "
+# sent = " 설치를 하고 싶은데 원격가능 할까요? "
 
 # 전처리 객체 생성
 p = Preprocess(userdic='../util/user_dic.tsv')
