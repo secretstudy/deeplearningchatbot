6740cd7317e216d7fedcb26a16efe143ece4274b
secretaistudy <assassinaideveloper@gmail.com>
2023-07-18 23:48:32 +0900

    Update Database.py

--- util/Database.py
+++ util/Database.py
@@ -6,3 +6,11 @@ class Database:
     '''
 
      '''
+
+    def __init__(self,host,user,password,db_name, charset='utf-8'):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.charset = charset
+        self.db_name = db_name
+        self.conn = None
