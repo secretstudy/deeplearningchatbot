654d590153c79e9a267e4331402a3073bb56542a
secretaistudy <assassinaideveloper@gmail.com>
2023-12-04 22:40:06 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,48 @@
+import socket
+import json
+import pymysql
+from config.DatabaseConfig import *
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# 챗봇 엔진 서버 접속 정보
+host = "127.0.0.1"  # 챗봇 엔진 서버 IP 주소
+port = 5050  # 챗봇 엔진 서버 통신 포트
+
+# MySQL 연결 설정
+mydb = pymysql.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_NAME
+)
+
+@app.route('/')
+def index():
+    return render_template('chat_page.html')  # HTML 페이지 렌더링
+
+@app.route('/chat_service/', methods=['POST'])
+def chat_service():
+    query = request.form['input1']  # 클라이언트로부터 입력 받기
+
+    # 챗봇 엔진 서버 연결
+    mySocket = socket.socket()
+    mySocket.connect((host, port))
+
+    json_data = {
+        'Query': query,
+        'BotType': "chat_service",
+    }
+
+    message = json.dumps(json_data)
+    mySocket.send(message.encode())
+
+    # 챗봇 엔진 답변 출력
+    data = mySocket.recv(2048).decode()
+
+    response = json.loads(data)
+
+    return jsonify(response)  # JSON 형태로 응답 반환
+if __name__ == '__main__':
+    app.run(debug=True)  # 서버 실행
